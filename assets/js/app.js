7b73ea76e3b98f863798db428f2a3792a1ac419c
Merlin Webster <mjftwebster@gmail.com>
2020-11-30 09:50:19 +0000

    Added basic stateless chat functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,30 @@ import "../css/app.scss"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-//     import socket from "./socket"
 //
+import socket from "./socket";
 import "phoenix_html"
+
+const channel = socket.channel("room:lobby", {});
+
+channel.on("shout", ({ name = "guest", message }) => {
+  const newMsg = document.createElement("li");
+  newMsg.innerHTML = `<b>${name}</b>: ${message}`;
+  msgList.appendChild(newMsg);
+});
+
+channel.join();
+
+const msgList = document.getElementById("msg-list");
+const msgInput = document.getElementById("msg");
+const nameInput = document.getElementById("name");
+
+msgInput.addEventListener("keypress", (event) => {
+  if (event.key == "Enter" && msgInput.value.length > 0) {
+    channel.push("shout", {
+      name: nameInput.value,
+      message: msgInput.value,
+    });
+    msgInput.value = "";
+  }
+});
